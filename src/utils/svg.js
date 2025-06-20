08042d04695d6035d1f2d486f3e1d2c4fac4e83c
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2025-06-20 21:45:53 +0200

    handle error on render svg cb

--- src/utils/svg.js
+++ src/utils/svg.js
@@ -3,13 +3,17 @@ export function renderSvgToDataURL(svgString, width, height) {
     const img = new Image();
 
     img.onload = () => {
-      const canvas = document.createElement("canvas");
-      canvas.width = width;
-      canvas.height = height;
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(img, 0, 0, width, height);
-      const dataURL = canvas.toDataURL("image/png");
-      resolve(dataURL);
+      try {
+        const canvas = document.createElement("canvas");
+        canvas.width = width;
+        canvas.height = height;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+        const dataURL = canvas.toDataURL("image/png");
+        resolve(dataURL);
+      } catch (err) {
+        reject(err);
+      }
     };
 
     img.onerror = (err) => {
