7179de865dbd24bc2374c0a089f0b65c530e9973
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2025-06-20 21:42:08 +0200

    fix set-icon

---
+++ src/utils/svg.js
@@ -0,0 +1,23 @@
+export function renderSvgToDataURL(svgString, width, height) {
+  console.log(svgString);
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0, width, height);
+      const dataURL = canvas.toDataURL("image/png");
+      resolve(dataURL);
+    };
+
+    img.onerror = (err) => {
+      console.log(err);
+      reject(err);
+    };
+
+    img.src = svgString;
+  });
+}
