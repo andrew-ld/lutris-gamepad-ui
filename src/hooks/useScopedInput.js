8be04a0969ed11ab59e12ff83f3b8019044fc73b
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2025-06-30 16:30:44 +0200

    fix

--- src/hooks/useScopedInput.js
+++ src/hooks/useScopedInput.js
@@ -5,7 +5,6 @@ export const useScopedInput = (handler, focusId, isActive = true) => {
   const { subscribe, claimInputFocus } = useInput();
   const inputTokenRef = useRef(null);
   const latestHandler = useRef(handler);
-  latestHandler.current = handler;
 
   useEffect(() => {
     if (isActive) {
