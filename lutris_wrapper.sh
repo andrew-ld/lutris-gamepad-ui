99fe76edf5b409d4757cfa551cc19264af12a73f
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2025-07-24 09:28:45 +0200

    use exec in lutris wrapper

--- lutris_wrapper.sh
+++ lutris_wrapper.sh
@@ -5,19 +5,20 @@ set -xe
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
+declare -a CMD_TO_RUN
+
 if [[ "$FLATPAK_ID" == "net.lutris.Lutris" ]]; then
-    python "$SCRIPT_DIR"/lutris_wrapper.py "$@"
-    exit $?
+    CMD_TO_RUN=(python "$SCRIPT_DIR/lutris_wrapper.py" "$@")
+
+elif command -v lutris &>/dev/null; then
+    CMD_TO_RUN=(python "$SCRIPT_DIR/lutris_wrapper.py" "$@")
+
+elif flatpak info net.lutris.Lutris &>/dev/null; then
+    CMD_TO_RUN=(flatpak run --command=bash --filesystem="$SCRIPT_DIR" net.lutris.Lutris "$SCRIPT_PATH" "$@")
+
+else
+    echo "Error: Lutris installation not found." >&2
+    exit 1
 fi
 
-if command -v lutris &>/dev/null; then
-    python "$SCRIPT_DIR"/lutris_wrapper.py "$@"
-    exit $?
-fi
-
-if flatpak info net.lutris.Lutris &>/dev/null; then
-    flatpak run --command=bash --filesystem="$SCRIPT_DIR" net.lutris.Lutris "$SCRIPT_PATH" "$@"
-    exit $?
-fi
-
-exit 1
+exec "${CMD_TO_RUN[@]}"
