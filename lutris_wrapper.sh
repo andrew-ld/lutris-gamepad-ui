b429e70b8b9128a6d95deb332bfb2c55e186d2d3
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2025-07-24 09:33:32 +0200

    prefer python3 instead of python command

--- lutris_wrapper.sh
+++ lutris_wrapper.sh
@@ -5,13 +5,22 @@ set -xe
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
+if command -v python3 &>/dev/null; then
+    PYTHON_CMD="python3"
+elif command -v python &>/dev/null; then
+    PYTHON_CMD="python"
+else
+    echo "Error: Could not find 'python3' or 'python' in your PATH." >&2
+    exit 1
+fi
+
 declare -a CMD_TO_RUN
 
 if [[ "$FLATPAK_ID" == "net.lutris.Lutris" ]]; then
-    CMD_TO_RUN=(python "$SCRIPT_DIR/lutris_wrapper.py" "$@")
+    CMD_TO_RUN=("$PYTHON_CMD" "$SCRIPT_DIR/lutris_wrapper.py" "$@")
 
 elif command -v lutris &>/dev/null; then
-    CMD_TO_RUN=(python "$SCRIPT_DIR/lutris_wrapper.py" "$@")
+    CMD_TO_RUN=("$PYTHON_CMD" "$SCRIPT_DIR/lutris_wrapper.py" "$@")
 
 elif flatpak info net.lutris.Lutris &>/dev/null; then
     CMD_TO_RUN=(flatpak run --command=bash --filesystem="$SCRIPT_DIR" net.lutris.Lutris "$SCRIPT_PATH" "$@")
