f7dbe515f4370c04910dcbb50d47c99f247ac768
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2026-03-10 21:04:56 +0100

    step 4 for display management from the ui

---
+++ src_backend/display_manager_impl_ddcutil.cjs
@@ -0,0 +1,44 @@
+const { spawnDdcutil, getRunExclusive } = require("./utils.cjs");
+
+const runExclusive = getRunExclusive()
+
+async function getBrightnessInternal() {
+  // format: VCP <code-hex> <type> <current-value> <max-value>
+  const result = await spawnDdcutil(["getvcp", "10", "--brief"]);
+  const parts = result.split("\n")[0].split(" ")
+
+  const current = parseInt(parts[3])
+  const max = parseInt(parts[4])
+
+  return { current, max }
+}
+
+async function getBrightness() {
+  return runExclusive(async () => {
+    const current = await getBrightnessInternal();
+    return Math.floor((current.current / current.max) * 100);
+  });
+}
+
+async function setBrightness(brightness) {
+  return runExclusive(async () => {
+    const current = await getBrightnessInternal();
+    const newVal = Math.floor((brightness / 100) * current.max);
+    await spawnDdcutil(["setvcp", "10", newVal.toString()]);
+  });
+}
+
+async function getNightLight() {
+  throw new Error("Night light reading not supported on this environment");
+}
+
+async function setNightLight() {
+  throw new Error("Night light control not supported on this environment");
+}
+
+module.exports = {
+  getBrightness,
+  setBrightness,
+  getNightLight,
+  setNightLight,
+};
