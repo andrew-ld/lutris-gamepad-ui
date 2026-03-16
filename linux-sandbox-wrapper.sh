846324946014b2d1a1a7d58734eb2ce314d09f5f
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2026-03-16 14:00:52 +0100

    Fix script variable handling

--- linux-sandbox-wrapper.sh
+++ linux-sandbox-wrapper.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 SOURCE="${BASH_SOURCE[0]}"
+
 while [ -h "$SOURCE" ]; do
-	SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
-	SOURCE="$(readlink "$SOURCE")"
-	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$SCRIPT_DIR/$SOURCE"
 done
 
 SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
-SCRIPT_NAME=$(basename "$0")
+SCRIPT_NAME="$(basename "$SOURCE")"
 
-exec "$SCRIPT_DIR/$SCRIPT_NAME.bin" "--no-sandbox" "$@"
+exec "$SCRIPT_DIR/$SCRIPT_NAME.bin" --no-sandbox "$@"
