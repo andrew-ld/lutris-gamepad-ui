8d03479558143bcc9f7726bf470cfbfae15c9d6e
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2026-03-29 17:33:35 +0200

    remove unusued koffie binaries

--- electron-builder.afterpack.cjs
+++ electron-builder.afterpack.cjs
@@ -1,9 +1,10 @@
 const fs = require("node:fs");
+const { readdir, rm } = require("node:fs/promises");
 const path = require("node:path");
 
 const { flipFuses, FuseV1Options, FuseVersion } = require("@electron/fuses");
 
-module.exports = async function afterPack(context) {
+async function disableSandboxWrapper(context) {
   const executableName = context.packager.executableName;
   const executablePath = path.join(context.appOutDir, executableName);
   const renamedPath = path.join(context.appOutDir, `${executableName}.bin`);
@@ -11,6 +12,15 @@ module.exports = async function afterPack(context) {
     context.packager.projectDir,
     "linux-sandbox-wrapper.sh",
   );
+
+  fs.renameSync(executablePath, renamedPath);
+  fs.copyFileSync(wrapperPath, executablePath);
+  fs.chmodSync(executablePath, 0o755);
+}
+
+async function patchElectronFuses(context) {
+  const executableName = context.packager.executableName;
+  const executablePath = path.join(context.appOutDir, executableName);
 
   await flipFuses(executablePath, {
     version: FuseVersion.V1,
@@ -23,8 +33,31 @@ module.exports = async function afterPack(context) {
     [FuseV1Options.LoadBrowserProcessSpecificV8Snapshot]: false,
     [FuseV1Options.GrantFileProtocolExtraPrivileges]: false,
   });
+}
 
-  fs.renameSync(executablePath, renamedPath);
-  fs.copyFileSync(wrapperPath, executablePath);
-  fs.chmodSync(executablePath, 0o755);
+const KOFFIE_ALLOWED_OS = new Set(["linux", "musl"]);
+
+async function removeKoffiUnusedBinaries(context) {
+  const koffiExecutablesPath = path.join(
+    context.appOutDir,
+    "resources/app.asar.unpacked/node_modules/koffi/build/koffi/",
+  );
+
+  const koffieExecutables = await readdir(koffiExecutablesPath);
+
+  for (const executable of koffieExecutables) {
+    const [os, arch] = executable.split("_");
+    const valid = KOFFIE_ALLOWED_OS.has(os) && arch == process.arch;
+
+    if (!valid) {
+      const executablePath = path.join(koffiExecutablesPath, executable);
+      await rm(executablePath, { recursive: true });
+    }
+  }
+}
+
+module.exports = async function afterPack(context) {
+  await patchElectronFuses(context);
+  await disableSandboxWrapper(context);
+  await removeKoffiUnusedBinaries(context);
 };
