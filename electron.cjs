c9d8f787e20eb0c45f4063a636d7a942129eaf30
NarkyAbyss <83636481+NarkyAbyss@users.noreply.github.com>
2026-06-16 16:56:59 +0800

    Update electron.cjs
    missing path import

--- electron.cjs
+++ electron.cjs
@@ -1,4 +1,5 @@
 const { app, Menu, protocol } = require("electron");
+const path = require("node:path");
 
 const customSessionDataPath = path.join(app.getPath("home"), ".cache", "lutris-gamepad-ui");
 fs.mkdirSync(customSessionPath, { recursive: true });
